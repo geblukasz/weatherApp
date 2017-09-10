2548f30c48454eea11b63ea80263540c6bbd21ba
Lukasz Gebicki <geblukasz@gmail.com>
2017-09-10 13:20:54 +0200

    after merge

--- src/main/java/com/ak/controler/MainWindowControler.java
+++ src/main/java/com/ak/controler/MainWindowControler.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
-import sun.nio.ch.IOUtil;
+
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -24,27 +24,21 @@ public class MainWindowControler implements Initializable {
     private final static String hotImage = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQSmmmLN1JfEkCvdvbhIDsQw_xAJQ5QKjVxmQ0lXlq3pbJ-vts4";
     private final static String defaultImage = "https://www.heitkamp.senate.gov/public/_cache/files/c9eec4d7-0487-40b9-bc34-9cbf0b907081/nd-landscapes-11.jpg";
 
-    //referencje na kontrolki
-
     @FXML
     private ImageView image;
 
     @FXML
     private Label temperature;
 
-    /*@FXML
-    private Label temperatureF;
-*/
     public void initialize(URL location, ResourceBundle resources) {
         temperature.setText("Pobieranie danych pogody");
         try {
             URL url = new URL(surl);
-            //jsonowy root
+            //json root
             JSONObject json = new JSONObject(IOUtils.toString(url));
-            //chcemy pobrac zawartosc taga o nazwie "current"
             JSONObject json2 = (JSONObject) json.get("current");
 
-            //pobieramy temperature w postaci tekstowej w *c
+            //temperature in text file in *c
             String sstemp = json2.get("temp_c").toString();
             Double temp = Double.parseDouble(sstemp);
 
@@ -64,7 +58,6 @@ public class MainWindowControler implements Initializable {
             }
 
             temperature.setText("Srednia temperatura *C: " + temp);
-            //temperatureF.setText("Srednia temperatura *C: " + ftemp);
             System.out.println(json.toString());
 
         } catch (MalformedURLException e) {
